2ab8ac960e8cae0b6a9b56317b08e8f0eeed5e7a
David <david.felley@students.hevs.ch>
2019-02-26 15:28:02 +0100

    First test github David

--- app/src/main/java/com/example/davidloris_project/MainActivity.java
+++ app/src/main/java/com/example/davidloris_project/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.davidloris_project;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivityLoris extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
