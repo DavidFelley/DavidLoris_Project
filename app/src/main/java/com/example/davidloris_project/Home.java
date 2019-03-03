19e15c7ae756b9d19c73ae7b018d15fcaab947a9
Loris_Clivaz <loris.clivaz@students.hevs.ch>
2019-03-03 14:56:49 +0100

    Création de toutes les méthodes concernant les genres Loris

--- app/src/main/java/com/example/davidloris_project/Home.java
+++ app/src/main/java/com/example/davidloris_project/Home.java
@@ -1,7 +1,9 @@
 package com.example.davidloris_project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Home extends AppCompatActivity {
 
@@ -9,5 +11,47 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void Actuality(View view)
+    {
+        Intent intent = new Intent(this, HomeCategory.class);
+        startActivity(intent);
+    }
+
+    public void Automobile(View view)
+    {
+        Intent intent = new Intent(this, HomeCategory.class);
+        startActivity(intent);
+    }
+
+    public void Game(View view)
+    {
+        Intent intent = new Intent(this, HomeCategory.class);
+        startActivity(intent);
+    }
+
+    public void Healthy(View view)
+    {
+        Intent intent = new Intent(this, HomeCategory.class);
+        startActivity(intent);
+    }
+
+    public void Informatique(View view)
+    {
+        Intent intent = new Intent(this, HomeCategory.class);
+        startActivity(intent);
+    }
+
+    public void Sexuality(View view)
+    {
+        Intent intent = new Intent(this, HomeCategory.class);
+        startActivity(intent);
+    }
+
+    public void Sport(View view)
+    {
+        Intent intent = new Intent(this, HomeCategory.class);
+        startActivity(intent);
     }
 }
