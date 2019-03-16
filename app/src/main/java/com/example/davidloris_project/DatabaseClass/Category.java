af222fb6a32031f7bb8671cde028a26ff3ed2e7c
Loris_Clivaz <loris.clivaz@students.hevs.ch>
2019-03-16 17:58:45 +0100

    Database creation class table

--- app/src/main/java/com/example/davidloris_project/DatabaseClass/Category.java
+++ app/src/main/java/com/example/davidloris_project/DatabaseClass/Category.java
@@ -1,8 +1,22 @@
 package com.example.davidloris_project.DatabaseClass;
 
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
 
+@Entity(tableName = "category")
 public class Category
 {
+
+    @NonNull
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    private int idCategory;
+
+    @ColumnInfo(name = "username")
+    private String categoryName;
+
 
 
 
--- app/src/main/java/com/example/davidloris_project/DatabaseClass/Subject.java
+++ app/src/main/java/com/example/davidloris_project/DatabaseClass/Subject.java
@@ -1,4 +1,30 @@
 package com.example.davidloris_project.DatabaseClass;
 
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+@Entity(tableName = "subject")
+
 public class Subject {
+
+    @NonNull
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "idSubject")
+    private int id;
+
+    @ColumnInfo(name = "titleSubject")
+    private String username;
+
+    @ColumnInfo(name = "textSubject")
+    private  String text;
+
+    @ColumnInfo (name = "idAutor")
+    private int idAutor;
+
+    @ColumnInfo (name = "idGenre")
+    private int idGenre;
+
+
 }
