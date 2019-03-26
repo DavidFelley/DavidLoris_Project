6e6972634535a670274325db271f994571593e50
David <david.felley@students.hevs.ch>
2019-03-26 13:56:06 +0100

    Request username
    Signed-off-by: David <david.felley@students.hevs.ch>

--- app/src/main/java/com/example/davidloris_project/Fragment/AboutFragment.java
+++ app/src/main/java/com/example/davidloris_project/Fragment/AboutFragment.java
@@ -1,4 +1,4 @@
-package com.example.davidloris_project;
+package com.example.davidloris_project.Fragment;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -7,6 +7,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.davidloris_project.R;
 
 public class AboutFragment extends Fragment
 {
